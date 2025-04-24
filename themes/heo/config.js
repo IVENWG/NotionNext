b1de22d42ce450750857004c0fa1076ee849d45c
IVENWG <39328997+IVENWG@users.noreply.github.com>
2025-04-24 14:51:41 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://qyj.xyz' },
+    { title: '欢迎来到我的网站', url: 'https://qyj.xyz' },
     { title: '我的YouTube', url: 'https://www.youtube.com/@Yitalks' },
     { title: '我的Blibli', url: 'https://space.bilibili.com/332437959' }
   ],
@@ -30,8 +30,8 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_2: { title: '热门视频', url: '/tag/热门视频' },
+  HEO_HERO_CATEGORY_3: { title: '热门文章', url: '/tag/热门文章' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -121,7 +121,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://t.me/qyjxyz',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
